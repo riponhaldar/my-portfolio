cc8ecfb27c57980ab6be5cf323d83130b5b8aa22
Ripon Haldar <mackparker001@gmail.com>
2021-09-20 21:59:31 +0530

    'headerres'

--- src/components/fontpage/Fontpage.js
+++ src/components/fontpage/Fontpage.js
@@ -3,13 +3,14 @@ import { useEffect, useRef } from 'react';
 import './header.scss';
 function Fontpage() {
   let backtext = useRef();
+  let headermini = useRef();
   let tl = new TimelineLite({ delay: 0.4 });
   useEffect(() => {
     gsap.from(
       backtext.children,
       1.2,
       {
-        y: 190,
+        y: 200,
         ease: Power3.out,
         delay: 0.3,
         stagger: {
@@ -27,28 +28,46 @@ function Fontpage() {
       stagger: {
         amount: 0.8,
       },
-    });
+    }).from(
+      headermini.children,
+      1.8,
+      {
+        y: 20,
+        ease: Power3.out,
+        delay: 0.3,
+        opacity: 0,
+        stagger: {
+          amount: 0.8,
+        },
+      },
+      '-=1.2'
+    );
   });
   return (
     <>
       <div className='header'>
-        <div className='back_text'>
-          <div ref={(el) => (backtext = el)}>
-            <p>CREATIVE</p>
+        <div className='header_a'>
+          <div className='back_text'>
+            <div ref={(el) => (backtext = el)}>
+              <p>CREATIVE</p>
+            </div>
           </div>
-        </div>
-        <div className='container'>
-          <div className='header__text'>
-            <div className='line'>
-              <p> Hi,</p>
-              <p> MY NAME IS RIPon </p>
-              <p className='deg'>,</p>
+          <div className='container'>
+            <div className='header__text'>
+              <div className='line'>
+                <p> Hi,</p>
+                <p> MY NAME IS RIPon, </p>
+              </div>
+              <div className='line'>
+                <p> I'm FRONTEND</p>
+              </div>
+              <div className='line'>
+                <p> -DEVELOPER</p>
+              </div>
             </div>
-            <div className='line'>
-              <p> I'm FRONTEND</p>
-            </div>
-            <div className='line'>
-              <p> -DEVELOPER</p>
+            <div className='header_mini' ref={(el) => (headermini = el)}>
+              <p>wellcome to my portfolio ,im students, </p>
+              <p>builds some project</p>
             </div>
           </div>
         </div>
